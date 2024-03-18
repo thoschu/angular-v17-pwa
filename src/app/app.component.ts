fa15f350581661791855d64b7b8deda1250dd1a4
Tom S. <thoschulte@googlemail.com>
2024-03-19 00:39:37 +0100

    feat: add service worker version management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SwUpdate, VersionEvent} from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,33 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'angular-v17-pwa';
+export class AppComponent implements AfterViewInit, OnInit {
+  protected readonly title: string = 'angular-v17-pwa 18';
+  protected disabled: boolean = true;
+
+  @ViewChild('map')
+  private readonly map?: ElementRef<HTMLDivElement> ;
+
+  constructor(private readonly swUpdate: SwUpdate) {}
+
+  protected async checkForUpdate(): Promise<void> {
+    window.location.reload();
+    this.disabled = true;
+  }
+
+  ngAfterViewInit(): void {
+    // console.log(this.map?.nativeElement);
+  }
+
+  ngOnInit(): void {
+    if(this.swUpdate.isEnabled) {
+      this.swUpdate.versionUpdates.subscribe((event: VersionEvent) => {
+        console.log(event);
+
+        if(event.type === 'VERSION_DETECTED') {
+          this.disabled = false;
+        }
+      });
+    }
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
 import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideServiceWorker } from '@angular/service-worker';
 
 import { routes } from './app.routes';
-import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
