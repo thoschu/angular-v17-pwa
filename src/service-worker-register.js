05a2100e861ee75ba80f1b094973bec6a4894870
Tom S. <thoschulte@googlemail.com>
2024-03-21 22:24:19 +0100

    refactor: further preparations for custom service worker

--- src/service-worker-register.js
+++ src/service-worker-register.js
@@ -1,15 +1,13 @@
-console.log('#####-A-#####');
-
 if('serviceWorker' in navigator) {
   const serviceWorkerRegistration = window.navigator.serviceWorker.register('/service-worker.js', {
     scope: '/',
     //scope: '/api'
   });
 
-  serviceWorkerRegistration.then( (registration) => {
+  serviceWorkerRegistration.then(registration => {
       console.log('Service Worker Registered', registration);
     })
-    .catch( (err) => {
+    .catch(err => {
       console.error('Service Worker Registration Failed', err);
     });
 }
