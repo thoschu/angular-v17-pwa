2ae720f5a0bf4b59bd91e3aadbdcd1282a8693e3
Tom S. <thoschulte@googlemail.com>
2024-03-22 02:46:47 +0100

    feat: add logic custom service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,11 +1,29 @@
-const VERSION = 'v6';
+const VERSION = 'v16';
 
 function log(...arguments) {
   console.log(`${VERSION}: `, ...arguments);
 }
 
 self.addEventListener('install', event => {
-  log(`⭐ Service-Worker installed.`, event);
+  log(`⭐ Service-Worker installation started.`, event);
+  const fn = async () => {
+    const request = new Request('offline.html');
+    const response = await fetch(request);
+
+    log(`⭐⭐ Service-Worker installation process loading: offline.html.`, request);
+
+    if(response.status !== 200) {
+      log(`⭐⭐⭐ Service-Worker installation process stopped: offline.html.`, response);
+      throw new Error(`Failed to load offline page: ${response.status}`, { cause: response.statusText });
+    }
+
+    log(`⭐⭐⭐ Service-Worker installation process loaded and cached: offline.html.`, response);
+
+    const cache = await caches.open(`static-app-cache`);
+    return await cache.put(request, response);
+  };
+
+  event.waitUntil(fn());
 });
 
 self.addEventListener('activate', event => {
@@ -14,8 +32,13 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   const url = new URL(event.request.url);
+  const fn = async () => {
+    log(`📎 Service Worker is fetching: ${url}`, event);
 
-  log(`⭕ Service Worker fetched.`, url);
+    return fetch(event.request);
+  };
+
+  event.respondWith(fn());
 });
 
 log(`❎ Service-Worker registered.`);
