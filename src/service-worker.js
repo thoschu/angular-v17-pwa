de377923409d92fd548b6cb82f8e766d69c339e1
Tom S. <thoschulte@googlemail.com>
2024-03-22 21:55:55 +0100

    refactor: cleanup

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,13 +2,13 @@ const VERSION = 'v1';
 
 self.addEventListener('install', event => {
   console.log('###');
-  const fn = async () => {
-    const cache = await caches.open(`static-app-cache-${VERSION}`);
-
-    return await cache.put(request, response);
-  };
-
-  event.waitUntil(fn());
+  // const fn = async () => {
+  //   const cache = await caches.open(`static-app-cache-${VERSION}`);
+  //
+  //   return await cache.put(request, response);
+  // };
+  //
+  // event.waitUntil(fn());
 });
 
 self.addEventListener('activate', event => {
@@ -17,5 +17,5 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   console.log('...');
-  event.respondWith(async () => {});
+  // event.respondWith(async () => {});
 });
